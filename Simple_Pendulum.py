5559d2eba526e50a71068a0e7d56a3b7ba387f68
AtharvaCPatil <88106686+AtharvaCPatil@users.noreply.github.com>
2021-09-09 21:46:32 +0530

    Simple Pendulum Animation w/o bob
    Subplots:
    graphs of x coordinate versus time
    graphs of y coordinate versus time
    simple pendulum animation

--- Simple_Pendulum.py
+++ Simple_Pendulum.py
@@ -1,1 +1,55 @@
 
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+import math
+from scipy.integrate import odeint
+
+
+def sys_val(initial, t):
+    omega = initial[1]
+    theta = initial[0]
+    omega_prime = -(g / l) * math.sin(theta)
+    return omega, omega_prime
+
+
+def sp(i):  # function for animation
+    ax1.cla()
+    ax2.cla()
+    ax3.cla()
+    initial = [theta_0, omega_0]  # initial_angle, initial_omega
+    theta_values = odeint(sys_val, initial, t)
+    # print(theta_values)
+    x = l * np.sin(theta_values[:, 0])
+    y = (-l) * np.cos(theta_values[:, 0])
+    # plotting ax1 [pendulum animation]
+    ax1.set_title("PENDULUM ANIMATION")
+    ax1.plot([0, x[len(x) - 1]], [0, y[len(y) - 1]])  # rod
+    ax1.plot(x[len(x) - 1], y[len(y) - 1])
+    ax1.set_xlim(-l - (0.5 * l), l + (0.5 * l))
+    ax1.set_ylim(-l - (0.5 * l), 0)
+    if len(t) > 5:
+        x_path = [x[len(x) - 1], x[len(x) - 2], x[len(x) - 3], x[len(x) - 4], x[len(x) - 5]]
+        y_path = [y[len(y) - 1], y[len(y) - 2], y[len(y) - 3], y[len(y) - 4], y[len(y) - 5]]
+        ax1.plot(x_path, y_path)
+
+    # plotting ax2 [x vs t] & ax3 [y vs t]
+    ax2.plot(t, x)
+    ax3.plot(t, x)
+    ax2.set_title("x v/s t")
+    ax3.set_title("y v/s t")
+
+    # append
+    t.append(t[len(t) - 1] + 0.05)
+
+
+t = [0.0]
+g = float(input("ENTER VALUE OF ACCLN DUE TO GRAVITY IN m/s*2:\n"))
+l = float(input("ENTER LENGTH OF PENDULUM IN m:\n"))
+theta_0 = float(input("ENTER INITIAL ANGLE OF PENDULUM IN radians:\n"))
+omega_0 = float(input("ENTER INITIAL ANGULAR VELOCITY OF PENDULUM IN radians/s:\n"))
+fig, (ax1, ax2, ax3) = plt.subplots(nrows=3, ncols=1)
+ani = FuncAnimation(fig, sp, interval=5)
+plt.show()
+plt.tight_layout()
+plt.axis("equal")
